1a87f262764796542c6a9bc577d6dd0daf0540a0
TascaBRX <lucastascasm23@gmail.com>
2025-08-21 17:36:47 -0300

    atualizando readme, gitignore e iniciando projeto

---
+++ Super_Trunfo/Cadastro cidades/Cadastro_cidades.c
@@ -0,0 +1,12 @@
+// objetivo é construir a base do nosso jogo Super Trunfo de Países: um sistema para cadastrar as cartas com informações sobre as cidades.
+// Instruções:
+// Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo. Para cada carta, o usuário deverá fornecer as seguintes informações:
+//    - Estado: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
+//    - Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
+//    - Nome da Cidade: O nome da cidade. Tipo: char[] (string)
+//    - População: Um número inteiro representando a população da cidade. Tipo: int
+//    - Área: Um número de ponto flutuante representando a área da cidade em km². Tipo: float
+//    - PIB: Um número de ponto flutuante representando o PIB da cidade.
+//    - Numero de Pontos turísticos: Um número inteiro representando o número de pontos turísticos da cidade. Tipo: int
+
+#include <stdio.h>
